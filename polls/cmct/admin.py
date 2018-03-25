4506051d230f4c9d1c41ca4bdd4df3a5a9547bf9
Kevin_QQ <ezhqing@163.com>
2018-03-25 17:31:59 +0800

    init

---
+++ polls/cmct/admin.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
---
+++ polls/cmct/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CmctConfig(AppConfig):
+    name = 'polls.cmct'
+    verbose_name = "Cmct"
